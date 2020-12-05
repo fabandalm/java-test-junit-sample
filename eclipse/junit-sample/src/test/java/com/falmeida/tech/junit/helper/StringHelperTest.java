6afd8bf9045228dd012f5851d2c8e1cf1bee138d
Fabio Almeida <falmeida.tech@gmail.com>
2020-12-04 22:00:05 -0500

    Added a sample junit test

---
+++ eclipse/junit-sample/src/test/java/com/falmeida/tech/junit/helper/StringHelperTest.java
@@ -0,0 +1,14 @@
+package com.falmeida.tech.junit.helper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringHelperTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
